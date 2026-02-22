5748f4312174e0e7ba9c2a1ada827f9c6cb0d22f
RiceCakes27 <84147811+RiceCakes27@users.noreply.github.com>
2026-02-21 22:07:52 -0500

    ah i understand the when x need y situation now

--- script.js
+++ script.js
@@ -523,13 +523,14 @@ function update() {
             switch (resolveWallCollision(playerRect, wall, 410)) {
                 case 'x':
                     speed.x *= -1; // Reverse x direction on collision
-                    speed.y *= -1;
                     player.style.left = Math.max(containerLeft-leftOffset, Math.min(playerLeft-leftOffset + speed.x, containerRight - playerWidth)) + 'px'; // Clamp position
                     playSound('WallBump2');
+                break;
                 case 'y':
                     speed.y *= -1; // Reverse y direction on collision
                     player.style.top = Math.max(containerTop, Math.min(playerTop + speed.y, containerBottom - playerHeight)) + 'px'; // Clamp position
                     playSound('WallBump2');
+                break;
             }
         });
     } else {
